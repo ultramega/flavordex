7a73d783e935bf6ff77aa37c77f42a3e3330e57f
Steve Guidetti <sguidetti@ultramegasoft.com>
2015-08-15 10:44:11 -0700

    Use old style inserts for compatibility

--- app/src/main/res/raw/schema.sql
+++ app/src/main/res/raw/schema.sql
@@ -34,29 +34,49 @@ CREATE TABLE `extras` (
   `deleted` INTEGER
 );
 --
-INSERT INTO `extras` VALUES
-(1, 1, '_style', 1, 0),
-(2, 1, '_serving', 1, 0),
-(3, 1, '_stats_ibu', 1, 0),
-(4, 1, '_stats_abv', 1, 0),
-(5, 1, '_stats_og', 1, 0),
-(6, 1, '_stats_fg', 1, 0),
-(7, 2, '_varietal', 1, 0),
-(8, 2, '_stats_vintage', 1, 0),
-(9, 2, '_stats_abv', 1, 0),
-(10, 3, '_style', 1, 0),
-(11, 3, '_stats_age', 1, 0),
-(12, 3, '_stats_abv', 1, 0),
-(13, 4, '_roaster', 1, 0),
-(14, 4, '_roast_date', 1, 0),
-(15, 4, '_grind', 1, 0),
-(16, 4, '_brew_method', 1, 0),
-(17, 4, '_stats_dose', 1, 0),
-(18, 4, '_stats_mass', 1, 0),
-(19, 4, '_stats_temp', 1, 0),
-(20, 4, '_stats_extime', 1, 0),
-(21, 4, '_stats_tds', 1, 0),
-(22, 4, '_stats_yield', 1, 0);
+INSERT INTO `extras` VALUES (1,1,'_style',1,0);
+--
+INSERT INTO `extras` VALUES (2,1,'_serving',1,0);
+--
+INSERT INTO `extras` VALUES (3,1,'_stats_ibu',1,0);
+--
+INSERT INTO `extras` VALUES (4,1,'_stats_abv',1,0);
+--
+INSERT INTO `extras` VALUES (5,1,'_stats_og',1,0);
+--
+INSERT INTO `extras` VALUES (6,1,'_stats_fg',1,0);
+--
+INSERT INTO `extras` VALUES (7,2,'_varietal',1,0);
+--
+INSERT INTO `extras` VALUES (8,2,'_stats_vintage',1,0);
+--
+INSERT INTO `extras` VALUES (9,2,'_stats_abv',1,0);
+--
+INSERT INTO `extras` VALUES (10,3,'_style',1,0);
+--
+INSERT INTO `extras` VALUES (11,3,'_stats_age',1,0);
+--
+INSERT INTO `extras` VALUES (12,3,'_stats_abv',1,0);
+--
+INSERT INTO `extras` VALUES (13,4,'_roaster',1,0);
+--
+INSERT INTO `extras` VALUES (14,4,'_roast_date',1,0);
+--
+INSERT INTO `extras` VALUES (15,4,'_grind',1,0);
+--
+INSERT INTO `extras` VALUES (16,4,'_brew_method',1,0);
+--
+INSERT INTO `extras` VALUES (17,4,'_stats_dose',1,0);
+--
+INSERT INTO `extras` VALUES (18,4,'_stats_mass',1,0);
+--
+INSERT INTO `extras` VALUES (19,4,'_stats_temp',1,0);
+--
+INSERT INTO `extras` VALUES (20,4,'_stats_extime',1,0);
+--
+INSERT INTO `extras` VALUES (21,4,'_stats_tds',1,0);
+--
+INSERT INTO `extras` VALUES (22,4,'_stats_yield',1,0);
 --
 CREATE TABLE `flavors` (
   `_id` INTEGER PRIMARY KEY,
@@ -65,66 +85,123 @@ CREATE TABLE `flavors` (
   `deleted` INTEGER
 );
 --
-INSERT INTO `flavors` VALUES
-(1, 1, 'Body', 0),
-(2, 1, 'Syrup', 0),
-(3, 1, 'Fruit', 0),
-(4, 1, 'Citrus', 0),
-(5, 1, 'Hops', 0),
-(6, 1, 'Linger', 0),
-(7, 1, 'Spice', 0),
-(8, 1, 'Herb', 0),
-(9, 1, 'Malt', 0),
-(10, 1, 'Alcohol', 0),
-(11, 1, 'Sweet', 0),
-(12, 1, 'Sour', 0),
-(13, 1, 'Bitter', 0),
-(14, 1, 'Astringent', 0),
-(15, 2, 'Body', 0),
-(16, 2, 'Fruit', 0),
-(17, 2, 'Citrus', 0),
-(18, 2, 'Berry', 0),
-(19, 2, 'Floral', 0),
-(20, 2, 'Spice', 0),
-(21, 2, 'Herb', 0),
-(22, 2, 'Nut', 0),
-(23, 2, 'Earth', 0),
-(24, 2, 'Wood', 0),
-(25, 2, 'Caramel', 0),
-(26, 2, 'Sweet', 0),
-(27, 2, 'Sour', 0),
-(28, 2, 'Astringent', 0),
-(29, 2, 'Linger', 0),
-(30, 2, 'Heat', 0),
-(31, 3, 'Body', 0),
-(32, 3, 'Charcoal', 0),
-(33, 3, 'Oak', 0),
-(34, 3, 'Leather', 0),
-(35, 3, 'Spice', 0),
-(36, 3, 'Alcohol', 0),
-(37, 3, 'Astringent', 0),
-(38, 3, 'Linger', 0),
-(39, 3, 'Sweet', 0),
-(40, 3, 'Maple', 0),
-(41, 3, 'Fruit', 0),
-(42, 3, 'Vanilla', 0),
-(43, 3, 'Smoke', 0),
-(44, 3, 'Peat', 0),
-(45, 3, 'Nut', 0),
-(46, 4, 'Body', 0),
-(47, 4, 'Citrus', 0),
-(48, 4, 'Berry', 0),
-(49, 4, 'Floral', 0),
-(50, 4, 'Spice', 0),
-(51, 4, 'Smoke', 0),
-(52, 4, 'Nut', 0),
-(53, 4, 'Chocolate', 0),
-(54, 4, 'Caramel', 0),
-(55, 4, 'Sweet', 0),
-(56, 4, 'Sour', 0),
-(57, 4, 'Bitter', 0),
-(58, 4, 'Salt', 0),
-(59, 4, 'Finish', 0);
+INSERT INTO `flavors` VALUES (1,1,'Body',0);
+--
+INSERT INTO `flavors` VALUES (2,1,'Syrup',0);
+--
+INSERT INTO `flavors` VALUES (3,1,'Fruit',0);
+--
+INSERT INTO `flavors` VALUES (4,1,'Citrus',0);
+--
+INSERT INTO `flavors` VALUES (5,1,'Hops',0);
+--
+INSERT INTO `flavors` VALUES (6,1,'Linger',0);
+--
+INSERT INTO `flavors` VALUES (7,1,'Spice',0);
+--
+INSERT INTO `flavors` VALUES (8,1,'Herb',0);
+--
+INSERT INTO `flavors` VALUES (9,1,'Malt',0);
+--
+INSERT INTO `flavors` VALUES (10,1,'Alcohol',0);
+--
+INSERT INTO `flavors` VALUES (11,1,'Sweet',0);
+--
+INSERT INTO `flavors` VALUES (12,1,'Sour',0);
+--
+INSERT INTO `flavors` VALUES (13,1,'Bitter',0);
+--
+INSERT INTO `flavors` VALUES (14,1,'Astringent',0);
+--
+INSERT INTO `flavors` VALUES (15,2,'Body',0);
+--
+INSERT INTO `flavors` VALUES (16,2,'Fruit',0);
+--
+INSERT INTO `flavors` VALUES (17,2,'Citrus',0);
+--
+INSERT INTO `flavors` VALUES (18,2,'Berry',0);
+--
+INSERT INTO `flavors` VALUES (19,2,'Floral',0);
+--
+INSERT INTO `flavors` VALUES (20,2,'Spice',0);
+--
+INSERT INTO `flavors` VALUES (21,2,'Herb',0);
+--
+INSERT INTO `flavors` VALUES (22,2,'Nut',0);
+--
+INSERT INTO `flavors` VALUES (23,2,'Earth',0);
+--
+INSERT INTO `flavors` VALUES (24,2,'Wood',0);
+--
+INSERT INTO `flavors` VALUES (25,2,'Caramel',0);
+--
+INSERT INTO `flavors` VALUES (26,2,'Sweet',0);
+--
+INSERT INTO `flavors` VALUES (27,2,'Sour',0);
+--
+INSERT INTO `flavors` VALUES (28,2,'Astringent',0);
+--
+INSERT INTO `flavors` VALUES (29,2,'Linger',0);
+--
+INSERT INTO `flavors` VALUES (30,2,'Heat',0);
+--
+INSERT INTO `flavors` VALUES (31,3,'Body',0);
+--
+INSERT INTO `flavors` VALUES (32,3,'Charcoal',0);
+--
+INSERT INTO `flavors` VALUES (33,3,'Oak',0);
+--
+INSERT INTO `flavors` VALUES (34,3,'Leather',0);
+--
+INSERT INTO `flavors` VALUES (35,3,'Spice',0);
+--
+INSERT INTO `flavors` VALUES (36,3,'Alcohol',0);
+--
+INSERT INTO `flavors` VALUES (37,3,'Astringent',0);
+--
+INSERT INTO `flavors` VALUES (38,3,'Linger',0);
+--
+INSERT INTO `flavors` VALUES (39,3,'Sweet',0);
+--
+INSERT INTO `flavors` VALUES (40,3,'Maple',0);
+--
+INSERT INTO `flavors` VALUES (41,3,'Fruit',0);
+--
+INSERT INTO `flavors` VALUES (42,3,'Vanilla',0);
+--
+INSERT INTO `flavors` VALUES (43,3,'Smoke',0);
+--
+INSERT INTO `flavors` VALUES (44,3,'Peat',0);
+--
+INSERT INTO `flavors` VALUES (45,3,'Nut',0);
+--
+INSERT INTO `flavors` VALUES (46,4,'Body',0);
+--
+INSERT INTO `flavors` VALUES (47,4,'Citrus',0);
+--
+INSERT INTO `flavors` VALUES (48,4,'Berry',0);
+--
+INSERT INTO `flavors` VALUES (49,4,'Floral',0);
+--
+INSERT INTO `flavors` VALUES (50,4,'Spice',0);
+--
+INSERT INTO `flavors` VALUES (51,4,'Smoke',0);
+--
+INSERT INTO `flavors` VALUES (52,4,'Nut',0);
+--
+INSERT INTO `flavors` VALUES (53,4,'Chocolate',0);
+--
+INSERT INTO `flavors` VALUES (54,4,'Caramel',0);
+--
+INSERT INTO `flavors` VALUES (55,4,'Sweet',0);
+--
+INSERT INTO `flavors` VALUES (56,4,'Sour',0);
+--
+INSERT INTO `flavors` VALUES (57,4,'Bitter',0);
+--
+INSERT INTO `flavors` VALUES (58,4,'Salt',0);
+--
+INSERT INTO `flavors` VALUES (59,4,'Finish',0);
 --
 CREATE TABLE `locations` (
   `_id` INTEGER PRIMARY KEY,
@@ -155,11 +232,13 @@ CREATE TABLE `types` (
   `preset` INTEGER
 );
 --
-INSERT INTO `types` VALUES
-(1, '_beer', 1),
-(2, '_wine', 1),
-(3, '_whiskey', 1),
-(4, '_coffee', 1);
+INSERT INTO `types` VALUES (1,'_beer',1);
+--
+INSERT INTO `types` VALUES (2,'_wine',1);
+--
+INSERT INTO `types` VALUES (3,'_whiskey',1);
+--
+INSERT INTO `types` VALUES (4,'_coffee',1);
 --
 CREATE TRIGGER `delete_entry` AFTER DELETE ON `entries`
 BEGIN
