4d84b58aa4be1779a3e3a2fb1036b6b949b4ecd5
Steve Guidetti <sguidetti@ultramegasoft.com>
2015-08-16 15:17:10 -0700

    Add table of extra fields

--- app/src/main/java/com/ultramegasoft/flavordex2/EntryInfoFragment.java
+++ app/src/main/java/com/ultramegasoft/flavordex2/EntryInfoFragment.java
@@ -2,6 +2,7 @@ package com.ultramegasoft.flavordex2;
 
 import android.content.ContentUris;
 import android.database.Cursor;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,10 +10,15 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RatingBar;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.ultramegasoft.flavordex2.provider.Tables;
@@ -28,6 +34,7 @@ import java.util.Locale;
  */
 public class EntryInfoFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     private static final int LOADER_MAIN = 0;
+    private static final int LOADER_EXTRAS = 1;
 
     /**
      * The database id for this entry
@@ -39,12 +46,15 @@ public class EntryInfoFragment extends Fragment implements LoaderManager.LoaderC
      */
     private TextView mTxtTitle;
     private RatingBar mRatingBar;
+
     private TextView mTxtMaker;
     private TextView mTxtOrigin;
     private TextView mTxtLocation;
     private TextView mTxtDate;
     private TextView mTxtPrice;
     private TextView mTxtNotes;
+
+    private TableLayout mTableExtras;
 
     public EntryInfoFragment() {
     }
@@ -63,6 +73,7 @@ public class EntryInfoFragment extends Fragment implements LoaderManager.LoaderC
         mRatingBar = (RatingBar)getActivity().findViewById(R.id.entry_rating);
 
         getLoaderManager().initLoader(LOADER_MAIN, null, this);
+        getLoaderManager().initLoader(LOADER_EXTRAS, null, this);
     }
 
     @Override
@@ -75,6 +86,8 @@ public class EntryInfoFragment extends Fragment implements LoaderManager.LoaderC
         mTxtDate = (TextView)rootView.findViewById(R.id.entry_date);
         mTxtPrice = (TextView)rootView.findViewById(R.id.entry_price);
         mTxtNotes = (TextView)rootView.findViewById(R.id.entry_notes);
+
+        mTableExtras = (TableLayout)rootView.findViewById(R.id.entry_extras);
 
         return rootView;
     }
@@ -125,6 +138,65 @@ public class EntryInfoFragment extends Fragment implements LoaderManager.LoaderC
     }
 
     /**
+     * Populates the table of extra fields.
+     *
+     * @param data The cursor containing the data
+     */
+    private void populateExtras(Cursor data) {
+        if(data.moveToFirst()) {
+            TableRow tableRow;
+            TextView textView;
+            View divider;
+
+            final int padding = getPixelValue(TypedValue.COMPLEX_UNIT_DIP, 4);
+
+            while(!data.isLast()) {
+                data.moveToNext();
+
+                tableRow = new TableRow(getActivity());
+
+                textView = new TextView(getActivity());
+                textView.setPadding(padding, 0, padding, 0);
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+                textView.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
+                textView.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+                textView.setText(data.getString(data.getColumnIndex(Tables.Extras.NAME)) + ": ");
+                tableRow.addView(textView);
+
+                textView = new TextView(getActivity());
+                textView.setPadding(padding, 0, padding, 0);
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+                textView.setTextIsSelectable(true);
+                textView.setText(data.getString(data.getColumnIndex(Tables.EntriesExtras.VALUE)));
+                tableRow.addView(textView);
+
+                divider = new View(getActivity());
+                divider.setLayoutParams(new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        getPixelValue(TypedValue.COMPLEX_UNIT_DIP, 1)));
+                divider.setBackgroundResource(android.R.drawable.divider_horizontal_dark);
+
+                mTableExtras.addView(divider);
+                mTableExtras.addView(tableRow);
+            }
+
+            mTableExtras.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * Convert a typed value to pixels.
+     *
+     * @param fromType  One of the unit constants in TypedValue
+     * @param fromValue The value to convert
+     * @return The pixel equivalent of the value as an integer
+     */
+    private int getPixelValue(int fromType, int fromValue) {
+        final DisplayMetrics metrics = getResources().getDisplayMetrics();
+        return (int)TypedValue.applyDimension(fromType, fromValue, metrics);
+    }
+
+    /**
      * Set the text of a TextView, replacing empty values to a placeholder.
      *
      * @param view  The view
@@ -143,10 +215,14 @@ public class EntryInfoFragment extends Fragment implements LoaderManager.LoaderC
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri uri = ContentUris.withAppendedId(Tables.Entries.CONTENT_ID_URI_BASE, mEntryId);
         switch(id) {
             case LOADER_MAIN:
-                final Uri uri = ContentUris.withAppendedId(Tables.Entries.CONTENT_ID_URI_BASE, mEntryId);
                 return new CursorLoader(getActivity(), uri, null, null, null, null);
+            case LOADER_EXTRAS:
+                uri = Uri.withAppendedPath(uri, "/extras");
+                return new CursorLoader(getActivity(), uri, null, null, null,
+                        Tables.Extras._ID + " ASC");
         }
 
         return null;
@@ -154,13 +230,19 @@ public class EntryInfoFragment extends Fragment implements LoaderManager.LoaderC
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        switch(loader.getId()) {
+        final int id = loader.getId();
+        switch(id) {
             case LOADER_MAIN:
                 if(data.moveToFirst()) {
                     populateViews(data);
                 }
                 break;
+            case LOADER_EXTRAS:
+                populateExtras(data);
+                getLoaderManager().destroyLoader(LOADER_EXTRAS);
         }
+
+        getLoaderManager().destroyLoader(id);
     }
 
     @Override
