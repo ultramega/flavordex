d077957ec958f1ca89a43be759a6ff5f0b4692b7
Steve Guidetti <sguidetti@ultramegasoft.com>
2015-08-22 14:29:09 -0700

    Use FragmentTabHost instead of action bar tabs

--- app/src/main/java/com/ultramegasoft/flavordex2/EntryDetailFragment.java
+++ app/src/main/java/com/ultramegasoft/flavordex2/EntryDetailFragment.java
@@ -1,13 +1,14 @@
 package com.ultramegasoft.flavordex2;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 import com.ultramegasoft.flavordex2.beer.BeerInfoFragment;
 import com.ultramegasoft.flavordex2.coffee.CoffeeInfoFragment;
@@ -20,7 +21,6 @@ import com.ultramegasoft.flavordex2.wine.WineInfoFragment;
  *
  * @author Steve Guidetti
  */
-@SuppressWarnings("deprecation")
 public class EntryDetailFragment extends Fragment {
     /**
      * The fragment argument representing the item ID that this fragment represents
@@ -41,19 +41,9 @@ public class EntryDetailFragment extends Fragment {
     private static final String TYPE_COFFEE = "_coffee";
 
     /**
-     * The saved state of the selected tab
-     */
-    private static final String STATE_SELECTED_TAB = "selected_tab";
-
-    /**
      * The database id for this entry
      */
     private long mEntryId;
-
-    /**
-     * Handle to the ActionBar
-     */
-    private ActionBar mActionBar;
 
     public EntryDetailFragment() {
     }
@@ -67,61 +57,32 @@ public class EntryDetailFragment extends Fragment {
     }
 
     @Override
-    public void onHiddenChanged(boolean hidden) {
-        super.onHiddenChanged(hidden);
-        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final FragmentTabHost tabHost =
+                (FragmentTabHost)inflater.inflate(R.layout.fragment_entry_detail, container, false);
+        tabHost.setup(getActivity(), getChildFragmentManager(), R.id.content);
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        final AppCompatActivity activity = (AppCompatActivity)getActivity();
-
-        mActionBar = activity.getSupportActionBar();
-
-        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        mActionBar.removeAllTabs();
-
-        ActionBar.Tab tab;
+        final Resources res = getResources();
         final Bundle args = new Bundle();
         args.putLong(ARG_ITEM_ID, mEntryId);
 
-        tab = mActionBar.newTab()
-                .setIcon(R.drawable.ic_description)
-                .setText(R.string.tab_info)
-                .setTabListener(new TabListener<>(activity, "info_" + mEntryId,
-                        getEntryInfoClass(), args));
-        mActionBar.addTab(tab);
+        Drawable icon;
+        TabHost.TabSpec tab;
 
-        tab = mActionBar.newTab()
-                .setIcon(R.drawable.ic_radar)
-                .setText(R.string.tab_chart)
-                .setTabListener(new TabListener<>(activity, "flavors_" + mEntryId,
-                        EntryFlavorsFragment.class, args));
-        mActionBar.addTab(tab);
+        icon = res.getDrawable(R.drawable.ic_description);
+        tab = tabHost.newTabSpec("info_" + mEntryId).setIndicator(null, icon);
+        tabHost.addTab(tab, getEntryInfoClass(), args);
 
-        tab = mActionBar.newTab()
-                .setIcon(R.drawable.ic_photo)
-                .setText(R.string.tab_photos)
-                .setTabListener(new TabListener<>(activity, "photos_" + mEntryId,
-                        EntryPhotosFragment.class, args));
-        mActionBar.addTab(tab);
+        icon = res.getDrawable(R.drawable.ic_radar);
+        tab = tabHost.newTabSpec("flavors_" + mEntryId).setIndicator(null, icon);
+        tabHost.addTab(tab, EntryFlavorsFragment.class, args);
 
-        if(savedInstanceState != null) {
-            mActionBar.setSelectedNavigationItem(savedInstanceState.getInt(STATE_SELECTED_TAB, 0));
-        }
-    }
+        icon = res.getDrawable(R.drawable.ic_photo);
+        tab = tabHost.newTabSpec("photos_" + mEntryId).setIndicator(null, icon);
+        tabHost.addTab(tab, EntryPhotosFragment.class, args);
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(STATE_SELECTED_TAB, mActionBar.getSelectedNavigationIndex());
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_entry_detail, container, false);
+        return tabHost;
     }
 
     /**
@@ -146,53 +107,5 @@ public class EntryDetailFragment extends Fragment {
         }
 
         return EntryInfoFragment.class;
-    }
-
-    /**
-     * Custom listener for action bar tabs.
-     *
-     * @param <T> A Fragment class
-     */
-    public static class TabListener<T extends Fragment> implements ActionBar.TabListener {
-        private Fragment mFragment;
-        private final AppCompatActivity mActivity;
-        private final String mTag;
-        private final Class<T> mClass;
-        private final Bundle mArgs;
-
-        /**
-         * @param activity The activity that the fragment belongs to
-         * @param tag      The tag to identify the fragment
-         * @param clz      The fragment class
-         * @param args     The arguments to pass to the fragment
-         */
-        public TabListener(AppCompatActivity activity, String tag, Class<T> clz, Bundle args) {
-            mActivity = activity;
-            mTag = tag;
-            mClass = clz;
-            mArgs = args;
-        }
-
-        @Override
-        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-            mFragment = mActivity.getSupportFragmentManager().findFragmentByTag(mTag);
-            if(mFragment == null) {
-                mFragment = Fragment.instantiate(mActivity, mClass.getName(), mArgs);
-                ft.add(R.id.content, mFragment, mTag);
-            } else {
-                ft.attach(mFragment);
-            }
-        }
-
-        @Override
-        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-            if(mFragment != null) {
-                ft.detach(mFragment);
-            }
-        }
-
-        @Override
-        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-        }
     }
 }
