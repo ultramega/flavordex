b22f0a94ae43afbabcec9c28d3ed0c3dc8bc4c15
Steve Guidetti <sguidetti@ultramegasoft.com>
2015-09-09 09:41:31 -0700

    Add Parcelable implementation to the BitmapCache

--- app/src/main/java/com/ultramegasoft/flavordex2/util/BitmapCache.java
+++ app/src/main/java/com/ultramegasoft/flavordex2/util/BitmapCache.java
@@ -1,28 +1,53 @@
 package com.ultramegasoft.flavordex2.util;
 
 import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.LruCache;
+
+import java.util.Map;
 
 /**
  * Memory cache for Bitmaps.
  *
  * @author Steve Guidetti
  */
-public class BitmapCache {
+public class BitmapCache implements Parcelable {
+    public static final Creator<BitmapCache> CREATOR = new Creator<BitmapCache>() {
+        @Override
+        public BitmapCache createFromParcel(Parcel in) {
+            return new BitmapCache(in);
+        }
+
+        @Override
+        public BitmapCache[] newArray(int size) {
+            return new BitmapCache[size];
+        }
+    };
+
     /**
      * The memory cache for storing data
      */
-    private final LruCache<Object, Bitmap> mCache;
+    private final LruCache<String, Bitmap> mCache;
 
     public BitmapCache() {
         final long maxMem = Runtime.getRuntime().maxMemory();
         final int cacheSize = (int)maxMem / 8;
-        mCache = new LruCache<Object, Bitmap>(cacheSize) {
+        mCache = new LruCache<String, Bitmap>(cacheSize) {
             @Override
-            protected int sizeOf(Object key, Bitmap value) {
+            protected int sizeOf(String key, Bitmap value) {
                 return value.getByteCount();
             }
         };
+    }
+
+    protected BitmapCache(Parcel in) {
+        this();
+        final Bundle bundle = in.readBundle();
+        for(String key : bundle.keySet()) {
+            put(key, (Bitmap)bundle.getParcelable(key));
+        }
     }
 
     /**
@@ -35,7 +60,7 @@ public class BitmapCache {
         if(key == null || bitmap == null) {
             return;
         }
-        mCache.put(key, bitmap);
+        mCache.put(key.toString(), bitmap);
     }
 
     /**
@@ -45,7 +70,7 @@ public class BitmapCache {
      * @return The Bitmap
      */
     public Bitmap get(Object key) {
-        return mCache.get(key);
+        return mCache.get(key.toString());
     }
 
     /**
@@ -54,6 +79,20 @@ public class BitmapCache {
      * @param key The key referencing the item
      */
     public void remove(Object key) {
-        mCache.remove(key);
+        mCache.remove(key.toString());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        final Bundle bundle = new Bundle();
+        for(Map.Entry<String, Bitmap> entry : mCache.snapshot().entrySet()) {
+            bundle.putParcelable(entry.getKey(), entry.getValue());
+        }
+        dest.writeBundle(bundle);
     }
 }
