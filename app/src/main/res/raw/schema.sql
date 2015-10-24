411e60ae27880eb8d36a41060736047cdf9bbde0
Steve Guidetti <sguidetti@ultramegasoft.com>
2015-10-24 11:03:52 -0700

    Remove back ticks

--- app/src/main/res/raw/schema.sql
+++ app/src/main/res/raw/schema.sql
@@ -1,84 +1,84 @@
-CREATE TABLE `entries` (
-  `_id` INTEGER PRIMARY KEY,
-  `cat` INTEGER,
-  `title` TEXT COLLATE NOCASE,
-  `maker` INTEGER,
-  `price` TEXT COLLATE NOCASE,
-  `location` TEXT COLLATE NOCASE,
-  `date` INTEGER,
-  `rating` REAL,
-  `notes` TEXT COLLATE NOCASE,
-  `updated` INTEGER,
-  `remote_id` INTEGER DEFAULT 0
+CREATE TABLE entries (
+  _id INTEGER PRIMARY KEY,
+  cat INTEGER,
+  title TEXT COLLATE NOCASE,
+  maker INTEGER,
+  price TEXT COLLATE NOCASE,
+  location TEXT COLLATE NOCASE,
+  date INTEGER,
+  rating REAL,
+  notes TEXT COLLATE NOCASE,
+  updated INTEGER,
+  remote_id INTEGER DEFAULT 0
 );
 --
-CREATE TABLE `entries_extras` (
-  `_id` INTEGER PRIMARY KEY,
-  `entry` INTEGER,
-  `extra` INTEGER,
-  `value` TEXT COLLATE NOCASE,
-  UNIQUE(`entry`, `extra`) ON CONFLICT REPLACE
+CREATE TABLE entries_extras (
+  _id INTEGER PRIMARY KEY,
+  entry INTEGER,
+  extra INTEGER,
+  value TEXT COLLATE NOCASE,
+  UNIQUE(entry, extra) ON CONFLICT REPLACE
 );
 --
-CREATE TABLE `entries_flavors` (
-  `_id` INTEGER PRIMARY KEY,
-  `entry` INTEGER,
-  `flavor` TEXT COLLATE NOCASE,
-  `value` INTEGER,
-  `pos` INTEGER
+CREATE TABLE entries_flavors (
+  _id INTEGER PRIMARY KEY,
+  entry INTEGER,
+  flavor TEXT COLLATE NOCASE,
+  value INTEGER,
+  pos INTEGER
 );
 --
-CREATE TABLE `extras` (
-  `_id` INTEGER PRIMARY KEY,
-  `cat` INTEGER,
-  `name` TEXT,
-  `pos` INTEGER,
-  `preset` INTEGER DEFAULT 0,
-  `deleted` INTEGER DEFAULT 0,
-  `remote_id` INTEGER DEFAULT 0
+CREATE TABLE extras (
+  _id INTEGER PRIMARY KEY,
+  cat INTEGER,
+  name TEXT,
+  pos INTEGER,
+  preset INTEGER DEFAULT 0,
+  deleted INTEGER DEFAULT 0,
+  remote_id INTEGER DEFAULT 0
 );
 --
-CREATE TABLE `flavors` (
-  `_id` INTEGER PRIMARY KEY,
-  `cat` INTEGER,
-  `name` TEXT,
-  `pos` INTEGER
+CREATE TABLE flavors (
+  _id INTEGER PRIMARY KEY,
+  cat INTEGER,
+  name TEXT,
+  pos INTEGER
 );
 --
-CREATE TABLE `locations` (
-  `_id` INTEGER PRIMARY KEY,
-  `lat` REAL,
-  `lon` REAL,
-  `name` TEXT,
-  UNIQUE(`lat`, `lon`) ON CONFLICT REPLACE
+CREATE TABLE locations (
+  _id INTEGER PRIMARY KEY,
+  lat REAL,
+  lon REAL,
+  name TEXT,
+  UNIQUE(lat, lon) ON CONFLICT REPLACE
 );
 --
-CREATE TABLE `makers` (
-  `_id` INTEGER PRIMARY KEY,
-  `name` TEXT COLLATE NOCASE,
-  `location`  TEXT COLLATE NOCASE,
-  UNIQUE(`name`, `location`) ON CONFLICT IGNORE
+CREATE TABLE makers (
+  _id INTEGER PRIMARY KEY,
+  name TEXT COLLATE NOCASE,
+  location  TEXT COLLATE NOCASE,
+  UNIQUE(name, location) ON CONFLICT IGNORE
 );
 --
-CREATE TABLE `photos` (
-  `_id` INTEGER PRIMARY KEY,
-  `entry` INTEGER,
-  `path` TEXT,
-  `drive_id` TEXT,
-  `pos` INTEGER
+CREATE TABLE photos (
+  _id INTEGER PRIMARY KEY,
+  entry INTEGER,
+  path TEXT,
+  drive_id TEXT,
+  pos INTEGER
 );
 --
-CREATE TABLE `cats` (
-  `_id` INTEGER PRIMARY KEY,
-  `name`  TEXT COLLATE NOCASE,
-  `preset` INTEGER DEFAULT 0,
-  `updated` INTEGER,
-  `remote_id` INTEGER DEFAULT 0
+CREATE TABLE cats (
+  _id INTEGER PRIMARY KEY,
+  name  TEXT COLLATE NOCASE,
+  preset INTEGER DEFAULT 0,
+  updated INTEGER,
+  remote_id INTEGER DEFAULT 0
 );
 --
-CREATE TABLE `deleted` (
-  `_id` INTEGER PRIMARY KEY,
-  `type` INTEGER,
-  `cat` INTEGER DEFAULT 0,
-  `remote_id` INTEGER
+CREATE TABLE deleted (
+  _id INTEGER PRIMARY KEY,
+  type INTEGER,
+  cat INTEGER DEFAULT 0,
+  remote_id INTEGER
 );
--- app/src/main/res/raw/triggers.sql
+++ app/src/main/res/raw/triggers.sql
@@ -1,40 +1,40 @@
-CREATE TRIGGER IF NOT EXISTS `delete_entry` AFTER DELETE ON `entries`
+CREATE TRIGGER IF NOT EXISTS delete_entry AFTER DELETE ON entries
 BEGIN
-    DELETE FROM `entries_flavors` WHERE `entry` = OLD.`_id`;
-    DELETE FROM `entries_extras` WHERE `entry` = OLD.`_id`;
-    DELETE FROM `photos` WHERE `entry` = OLD.`_id`;
-    DELETE FROM `makers` WHERE `_id` = OLD.`maker`
-     AND NOT EXISTS (SELECT 1 FROM `entries` WHERE `maker` = OLD.`maker`);
+    DELETE FROM entries_flavors WHERE entry = OLD._id;
+    DELETE FROM entries_extras WHERE entry = OLD._id;
+    DELETE FROM photos WHERE entry = OLD._id;
+    DELETE FROM makers WHERE _id = OLD.maker
+     AND NOT EXISTS (SELECT 1 FROM entries WHERE maker = OLD.maker);
 END;
 --
-CREATE TRIGGER IF NOT EXISTS `update_entry` AFTER UPDATE OF `maker` ON `entries`
+CREATE TRIGGER IF NOT EXISTS update_entry AFTER UPDATE OF maker ON entries
 BEGIN
-    DELETE FROM `makers` WHERE `_id` = OLD.`maker`
-     AND NOT EXISTS (SELECT 1 FROM `entries` WHERE `maker` = OLD.`maker`);
+    DELETE FROM makers WHERE _id = OLD.maker
+     AND NOT EXISTS (SELECT 1 FROM entries WHERE maker = OLD.maker);
 END;
 --
-CREATE TRIGGER IF NOT EXISTS `delete_cat` AFTER DELETE ON `cats`
+CREATE TRIGGER IF NOT EXISTS delete_cat AFTER DELETE ON cats
 BEGIN
-    DELETE FROM `entries` WHERE `cat` = OLD.`_id`;
-    DELETE FROM `extras` WHERE `cat` = OLD.`_id`;
-    DELETE FROM `flavors` WHERE `cat` = OLD.`_id`;
-    DELETE FROM `deleted` WHERE `cat` = OLD.`_id`;
+    DELETE FROM entries WHERE cat = OLD._id;
+    DELETE FROM extras WHERE cat = OLD._id;
+    DELETE FROM flavors WHERE cat = OLD._id;
+    DELETE FROM deleted WHERE cat = OLD._id;
 END;
 --
-CREATE TRIGGER IF NOT EXISTS `delete_entry_extra` AFTER DELETE ON `entries_extras`
+CREATE TRIGGER IF NOT EXISTS delete_entry_extra AFTER DELETE ON entries_extras
 BEGIN
-    DELETE FROM `extras` WHERE `deleted` = 1
-     AND NOT EXISTS (SELECT 1 FROM `entries_extras` WHERE `extra` = `extras`.`_id`);
+    DELETE FROM extras WHERE deleted = 1
+     AND NOT EXISTS (SELECT 1 FROM entries_extras WHERE extra = extras._id);
 END;
 --
-CREATE TRIGGER IF NOT EXISTS `update_entry_extra` AFTER UPDATE OF `extra` ON `entries_extras`
+CREATE TRIGGER IF NOT EXISTS update_entry_extra AFTER UPDATE OF extra ON entries_extras
 BEGIN
-    DELETE FROM `extras` WHERE `deleted` = 1
-     AND NOT EXISTS (SELECT 1 FROM `entries_extras` WHERE `extra` = `extras`.`_id`);
+    DELETE FROM extras WHERE deleted = 1
+     AND NOT EXISTS (SELECT 1 FROM entries_extras WHERE extra = extras._id);
 END;
 --
-CREATE TRIGGER IF NOT EXISTS `delete_extra` BEFORE DELETE ON `extras`
+CREATE TRIGGER IF NOT EXISTS delete_extra BEFORE DELETE ON extras
 BEGIN
-    UPDATE `extras` SET `deleted` = 1 WHERE `_id` = OLD.`_id`;
-    SELECT RAISE(IGNORE) WHERE EXISTS (SELECT 1 FROM `entries_extras` WHERE `extra` = OLD.`_id`);
+    UPDATE extras SET deleted = 1 WHERE _id = OLD._id;
+    SELECT RAISE(IGNORE) WHERE EXISTS (SELECT 1 FROM entries_extras WHERE extra = OLD._id);
 END;
