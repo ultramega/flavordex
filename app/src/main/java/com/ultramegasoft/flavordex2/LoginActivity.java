6be61e568d7f76b2411baaf21dc9feea8e4065b5
Steve Guidetti <sguidetti@ultramegasoft.com>
2016-07-18 16:30:34 -0700

    Remove action bar code

--- app/src/main/java/com/ultramegasoft/flavordex2/LoginActivity.java
+++ app/src/main/java/com/ultramegasoft/flavordex2/LoginActivity.java
@@ -5,11 +5,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -105,11 +103,6 @@ public class LoginActivity extends AppCompatActivity
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final ActionBar actionBar = getSupportActionBar();
-        if(actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-
         mAuth = FirebaseAuth.getInstance();
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -143,16 +136,6 @@ public class LoginActivity extends AppCompatActivity
         if(mAuthStateListener != null) {
             mAuth.removeAuthStateListener(mAuthStateListener);
         }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     /**
